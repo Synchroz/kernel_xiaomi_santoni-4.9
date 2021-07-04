e645985446f5539e4b12715b56a4291752e65463
zeta96 <ahmad.zainul857@gmail.com>
2021-07-04 10:15:04 +0000

    kernel : rename overcommit_ratio

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1424,7 +1424,14 @@ static struct ctl_table vm_table[] = {
 		.proc_handler	= proc_dointvec,
 	},
 	{
-		.procname	= "overcommit_ratio",
+		.procname       = "reap_mem_on_sigkill",
+		.data           = &sysctl_reap_mem_on_sigkill,
+		.maxlen         = sizeof(sysctl_reap_mem_on_sigkill),
+		.mode           = 0644,
+		.proc_handler   = proc_dointvec,
+	},
+	{
+		.procname	= "overcommit_ratio_nosys",
 		.data		= &sysctl_overcommit_ratio,
 		.maxlen		= sizeof(sysctl_overcommit_ratio),
 		.mode		= 0644,
