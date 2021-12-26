bf78a6360df197fac24d70c245d6a68fb796ef7a
Synchroz <juliyanto1071@student.untan.ac.id>
2021-12-26 19:39:39 +0700

    drm:dsi:staging : added high brigthness mode
    try adapted code from kernel 4.14, source code : https://github.com/GZR-Kernels/Optimus_Drunk_Surya/commit/7f949593df3e0c3348cfe128ae1196b425663ef6

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -217,6 +217,8 @@ struct dsi_panel {
 	bool sync_broadcast_en;
 
 	struct dsi_panel_exd_config exd_config;
+	
+	int hbm_mode;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -317,5 +319,7 @@ int dsi_panel_parse_esd_reg_read_configs(struct dsi_panel *panel,
 				struct device_node *of_node);
 
 void dsi_panel_ext_bridge_put(struct dsi_panel *panel);
+
+int dsi_panel_apply_hbm_mode(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
