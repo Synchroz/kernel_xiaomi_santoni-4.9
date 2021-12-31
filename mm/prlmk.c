b796b16fb70219563f7c4785d710f6c92023fe93
Synchroz <juliyanto1071@student.untan.ac.id>
2022-01-01 01:37:02 +0700

    tune prlmk(2)

--- mm/prlmk.c
+++ mm/prlmk.c
@@ -47,7 +47,7 @@ static inline void lowmem_dbg(unsigned long filepgs, ...) { }
 
 #define SWAP_EFF_WIN 2
 #define SWAP_OPT_EFF 50
-#define PER_SWAP_SIZE (SWAP_CLUSTER_MAX * 32)
+#define PER_SWAP_SIZE (SWAP_CLUSTER_MAX * 64)
 
 struct selected_task {
 	struct task_struct *p;
@@ -108,14 +108,14 @@ module_param_named(min_adj, min_adj, short, 0644);
  * too low, and task killing can be more aggressive when too
  * high.
  */
-static int free_file_limit = 37500;
+static int free_file_limit = 27500;
 module_param_named(free_file_limit, free_file_limit, int, 0644);
 
 /*
  * Number of SWAP pages in MiB below which tasks with
  * adjs greater than min_adj should be killed.
  */
-static int free_swap_limit = 50;
+static int free_swap_limit = 40;
 module_param_named(free_swap_limit, free_swap_limit, int, 0644);
 
 /*
